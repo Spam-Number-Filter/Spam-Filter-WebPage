0f3270a31b5b18f98c4723dfe6ed485435ff8fcb
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-11 16:15:30 +0100

    feat(data): create database models

--- dataNumbers/admin.py
+++ dataNumbers/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from dataNumbers.models import *
+from django.apps import apps
 
 # Register your models here.
+# models = apps.get_models()
+# for model in models:
+#     admin.site.register(model)
+
+admin.site.register(User)
+admin.site.register(Telephone)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(VoteCategory)
--- dataNumbers/models.py
+++ dataNumbers/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import Model
+
+NAME_MAX_LENGTH = 50
+PASSWORD_MAX_LENGTH = 20
+EMAIL_MAX_LENGTH = 100
+
+
+class User(Model):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    password = models.CharField(max_length=PASSWORD_MAX_LENGTH)
+    mail = models.EmailField(max_length=EMAIL_MAX_LENGTH, unique=True)
+
+
+class Telephone(Model):
+    telephone_id = models.AutoField(primary_key=True)
+    phone = models.IntegerField()
+    prefix = models.IntegerField()
+
+    class Meta:
+        unique_together = ('phone', 'prefix',)
+
+
+TITLE_MAX_LENGTH = 30
+
+
+class Post(Model):
+    post_id = models.AutoField(primary_key=True)
+    creation_user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    # TODO: Put that on documentation
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    telephone = models.ForeignKey(Telephone, on_delete=models.CASCADE)
+
+
+class Comment(Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+
+
+MAX_CATEGORY_LENGTH = 20
+
+
+class Category(Model):
+    type = models.CharField(max_length=MAX_CATEGORY_LENGTH, primary_key=True)
+
+
+class VoteCategory(Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
