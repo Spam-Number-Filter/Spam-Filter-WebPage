d78380f5e7ff8c3176ab853f62f25e33b6345a5f
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-14 10:51:20 +0100

    fix: error 500 on heroku
    Fix heroku not recognizing the static files

--- numberFilter/settings/settings.py
+++ numberFilter/settings/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
