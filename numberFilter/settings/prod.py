8127a8a5f6f644c7e52279d866cf14b0f9fb273b
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-03-14 10:11:14 +0100

    build: use postgresql in production

--- numberFilter/settings/prod.py
+++ numberFilter/settings/prod.py
@@ -1,7 +1,19 @@
 from .settings import *
 import django_heroku
+import os
 
 DEBUG = False
 ALLOWED_HOSTS = ["http://spamnumberfilter.herokuapp.com", "http://spamnumberfilter.herokuapp.com/"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST'),
+        'PORT': '5432',
+    }
+}
 
 django_heroku.settings(locals())
