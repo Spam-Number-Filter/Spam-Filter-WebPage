d4862fea71a918356624577beb38dc0bc98dbc90
Roger Fontova Torres <rogerfontovat@gmail.com>
2022-03-28 18:27:43 +0200

    feat(login): added css to login page

--- numberFilter/settings/settings.py
+++ numberFilter/settings/settings.py
@@ -35,7 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
