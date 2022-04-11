571de155f5ddd8e49468a438485b6ff4585eb629
FerranAD <farandomingo@gmail.com>
2022-04-11 21:14:59 +0200

    Revert "feat(login): added css to login page"
    This reverts commit d4862fea71a918356624577beb38dc0bc98dbc90.

--- number_filter/settings/base.py
+++ number_filter/settings/base.py
@@ -35,10 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crispy_forms",
 ]
-
-CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 
 MIDDLEWARE = [
