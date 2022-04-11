a9569745e6ca1a6ea29e70b78e6866efcd985ba0
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-04-11 22:26:47 +0200

    fix: partition html files depending on context
    It's a good practice to have different static folders depending
    on the business logic we're using.

--- data_numbers/urls.py
+++ data_numbers/urls.py
@@ -1,8 +1,9 @@
 """" Module that contains a lsit of URL patterns
 and which view should be run when they are requested. """
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
--- number_filter/urls.py
+++ number_filter/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
     path("", include("data_numbers.urls")),
 ]
