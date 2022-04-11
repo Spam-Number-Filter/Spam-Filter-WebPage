e944a593e37d3d7269de5eaaf41e15823663935e
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-04-11 23:20:13 +0200

    feat(login): add bootstraped login

--- data_numbers/urls.py
+++ data_numbers/urls.py
@@ -3,7 +3,10 @@ and which view should be run when they are requested. """
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from data_numbers.views import login_user
+
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
+    path("login/", login_user, name="login"),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
--- data_numbers/views.py
+++ data_numbers/views.py
@@ -1,9 +1,11 @@
 """ File that conatinas the controller of MVC,
 the code controlling the business logic of the application. """
-
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 
 # Create your views here.
+from django.shortcuts import redirect, render
 from django.template import loader
 
 
@@ -11,3 +13,19 @@ def index(request):
     """Controller of the index page."""
     template = loader.get_template("home.html")
     return HttpResponse(template.render())
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            messages.success(request, ("There was an error logging in"))
+            return redirect("login")
+    else:
+        print(request)
+        return render(request, "registration/login.html", {})
