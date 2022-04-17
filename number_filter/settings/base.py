32438b6bf189b94618d06f197b46629af7a9578f
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-04-17 19:58:56 +0200

    feat(password_reset): add local skeleton

--- number_filter/settings/base.py
+++ number_filter/settings/base.py
@@ -123,3 +123,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+# Reset password related settings, it would be ideal to have a separate
+# smtp server for this, but for now we'll save every email inside /sent_emails
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
